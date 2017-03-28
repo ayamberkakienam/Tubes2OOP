8261780f9cef14432c2f2cc2fd6a0144324009ef
freedomfeather <fadlurohmanakmal@rocketmail.com>
2017-03-28 16:04:40 +0700

    add ikanterbang

---
+++ MyZoo/src/animal/real/IkanTerbang.java
@@ -0,0 +1,28 @@
+package animal.real;
+
+import animal.classification.waterair.finned.insect.FinnedWAInsect;
+
+/**
+ * IkanTerbang
+ * Hewan amphibi yang dapat hidup di air dan udara
+ */
+public class IkanTerbang extends FinnedWAInsect {
+  /**
+   * Constructor.
+   * Melakukan inisialisasi kelas dengan parameter absis dan ordinat lokasi
+   */
+  public IkanTerbang(int i, int j) {
+    super((float) 2, i, j, true);
+    SetName("Ikan Terbang");
+    SetContent('I');
+    consumerate += GetFodder();
+  }
+  /**
+   * Mengembalikan string yang menggambarkan interaksi user dengan hewan
+   *
+   * @return String sesuai implementasi pada kelas riil turunan
+   */
+  public String Interact() {
+    return "Cepok cepok cepok";
+  }
+}
